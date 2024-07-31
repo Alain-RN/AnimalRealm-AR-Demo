079d8fc179dfbdb47c12a3347b21179657055a93
Noelisoa Alain <alainnoelisoa@gmail.com>
2024-07-31 23:53:22 +0300

    Deuxieme commit

--- app/src/main/java/mada/alain/animalrealm/MainActivity.kt
+++ app/src/main/java/mada/alain/animalrealm/MainActivity.kt
@@ -3,13 +3,18 @@ package mada.alain.animalrealm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import mada.alain.animalrealm.ui.theme.AnimalRealmTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +27,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SplashScreen(nameApp = "AnimalRealm", modifier = Modifier)
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+
+@Preview(
+    showBackground = true,
+    widthDp = 350
     )
-}
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun OpeningPreview() {
     AnimalRealmTheme {
-        Greeting("Android")
+
+    }
     }
 }
--- app/src/main/java/mada/alain/animalrealm/ui/theme/Color.kt
+++ app/src/main/java/mada/alain/animalrealm/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package mada.alain.animalrealm.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BeigeSable = Color(0xFFD4A373)
+val VertFeuille = Color(0xFF4A7F2C)
+val MarronTerreOmbre = Color(0xFF5C4033)
+val GrisArdoise = Color(0xFF6D6C71)
